1fc1c17f69c109a029eb2a276521c2f5cf7f147b
Adam Dudley <dudleyadam@gmail.com>
2018-11-19 20:49:59 +0000

    automatically backed up by learn

--- times.rb
+++ times.rb
@@ -1,10 +1,5 @@
 def using_times(levitation_charm)
-levitation_charm = "Wingardium Leviosa"
   7.times do
-	  puts levitation_charm
+	  puts levitation_charm("Wingardium Leviosa")
 	end
 end
-
-
-
-
